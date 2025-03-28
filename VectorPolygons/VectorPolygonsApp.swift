96884d05f2f63303b459e5ae7863554b626fef64
Miksa <malaxa2000@gmail.com>
2025-03-28 17:03:46 +0300

    Initial Commit

---
+++ VectorPolygons/VectorPolygonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  VectorPolygonsApp.swift
+//  VectorPolygons
+//
+//  Created by Michail Malashkevich on 28.03.25.
+//
+
+import SwiftUI
+
+@main
+struct VectorPolygonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
